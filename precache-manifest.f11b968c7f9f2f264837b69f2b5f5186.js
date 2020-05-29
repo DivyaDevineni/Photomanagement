d28ff31acc2123bb8f38c614a0ebd7bb52bf195c
divyadevineni <divyadevineni51@gmail.com>
2020-05-29 11:19:03 -0600

    Updates

---
+++ precache-manifest.f11b968c7f9f2f264837b69f2b5f5186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3813315fd650c9daf64ff2ca10e6dd",
+    "url": "/Photomanagement/index.html"
+  },
+  {
+    "revision": "490523b59187dce941af",
+    "url": "/Photomanagement/static/css/main.e8d1d53c.chunk.css"
+  },
+  {
+    "revision": "c71a29d8b38447c1469b",
+    "url": "/Photomanagement/static/js/2.03cc51a9.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Photomanagement/static/js/2.03cc51a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490523b59187dce941af",
+    "url": "/Photomanagement/static/js/main.98966633.chunk.js"
+  },
+  {
+    "revision": "4f9d0c541c4d3b09eb56",
+    "url": "/Photomanagement/static/js/runtime-main.5bb16399.js"
+  }
+]);
